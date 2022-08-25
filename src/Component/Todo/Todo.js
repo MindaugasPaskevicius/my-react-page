e0f0b8ac776e9c9bcceedcd384e1865317255387
MindaugasPaskevicius <106962850+MindaugasPaskevicius@users.noreply.github.com>
2022-08-25 08:40:48 +0300

    edit todo css

--- src/Component/Todo/Todo.js
+++ src/Component/Todo/Todo.js
@@ -54,7 +54,7 @@ const Todo = () => {
        <div className="d-flex justify-content-center mt-5">
          <div id="todo-card">
            <div className="card-body">
-            <h2 className="card-title text-center pt-5">List items</h2>
+            <h2 className="card-title text-center pt-5">Items list</h2>
             <form className="col-12 d-flex m-3 justify-content-center pe-3 pt-4">
               <div className="col-5 ">
                 <input
@@ -71,7 +71,8 @@ const Todo = () => {
                 </button>
               </div>
             </form>
-            <ul className="list-group ">
+            <div className="d-flex justify-content-center ">
+            <ul className="list-group">
               {items.length > 0 ? (
                 items.map((items, index) => (
                   <li key={index} className="list-group-item mt-2">
@@ -84,6 +85,7 @@ const Todo = () => {
                 <div className="text-center text-danger pt-5">No items found!</div>
               )}
             </ul>
+            </div>
           </div>
         
       </div>
